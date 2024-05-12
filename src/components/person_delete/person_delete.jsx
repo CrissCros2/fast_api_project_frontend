467a67d58783ecd9827ba9ab4a7c4c3671da97fb
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-05-12 14:32:16 +0100

    Add missing file

---
+++ src/components/person_delete/person_delete.jsx
@@ -0,0 +1,52 @@
+import Grid from '@mui/joy/Grid';
+import Sheet from '@mui/joy/Sheet';
+import { styled } from '@mui/joy/styles';
+import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+import Box from '@mui/joy/Box';
+import Button from "@mui/joy/Button";
+import React, {useState} from "react";
+import {DeletePerson} from "../../apiCalls";
+import {PersonSelect} from "../person_select/person_select";
+
+const CenteredItem = styled(Sheet)(() => ({
+    textAlign: 'center',
+    background: 'transparent'
+}));
+
+export function PersonDelete({slide_out, setPersonsUpdated, persons}) {
+    const [selectedPerson, setSelectedPerson] = useState([]);
+
+    const handleSubmit = (event) => {
+        DeletePerson(selectedPerson.id);
+        event.preventDefault();
+        slide_out();
+        setPersonsUpdated(true);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Box
+                sx={{width: '15%'}}
+                alignItems="center"
+                display="flex"
+                alignContent='center'
+                data-testid="create-person-box">
+                <Card variant="solid">
+                    <Grid direction="column" justifyContent="center" alignItems="center" container spacing={2} sx={{ flexGrow: 1 }}>
+                        <Grid xs={8}>
+                            <CenteredItem><Typography level="title-md" textColor="white">Delete Person:</Typography></CenteredItem>
+                        </Grid>
+                        <Grid xs={8}>
+                            <PersonSelect setInputs={setSelectedPerson} persons={persons} multiple={false}/>
+                        </Grid>
+                        <Grid xs={8}>
+                            <CenteredItem> <Button type='submit' variant="solid">Create Person</Button></CenteredItem>
+                        </Grid>
+                    </Grid>
+                </Card>
+            </Box>
+        </form>
+    );
+}
+
