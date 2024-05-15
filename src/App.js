b842dabae699bd4721bd7c079f83fcc3103e76ad
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-05-15 20:46:41 +0100

    Base calendar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Sliders } from './components/sliders/sliders';
+import Calendar from "./components/calendar/calendar";
 
 
 function App() {
   return (
       <div data-testid="test-app">
           <Sliders />
+          <Calendar />
       </div>
   );
 }
